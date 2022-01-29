fd8bbe9b2c907889d6f008edf23ed9ed2749dabf
enchilo <enchi850404@gmail.com>
2022-01-29 11:16:54 +0800

    chore: JSscript add async function

--- CrossClockIn.js
+++ CrossClockIn.js
@@ -1,20 +1,24 @@
 const newman = require('newman'); // require newman in your project
 
 // call newman.run to pass `options` object and wait for callback
-newman.run({
-    collection: require('./CrossClockIn.postman_collection.json'),
-    reporters: ['junit', 'cli'],
-    reporter: { junit: { export: "./newman/" } }
-}, function (err) {
-    if (err) { throw err; }
-    console.log('collection run complete!');
-});
+async function CrossClockIn() {
+    newman.run({
+        collection: require('./CrossClockIn.postman_collection.json'),
+        reporters: ['junit', 'cli'],
+        reporter: { junit: { export: "./newman" } }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
 
-newman.run({
-    collection: require('./HousekeeperToken.postman_collection.json'),
-    reporters: ['junit', 'cli'],
-    reporter: { junit: { export: "./newman/" } }
-}, function (err) {
-    if (err) { throw err; }
-    console.log('collection run complete!');
-});
+    newman.run({
+        collection: require('./HousekeeperToken.postman_collection.json'),
+        reporters: ['junit', 'cli'],
+        reporter: { junit: { export: "./newman" } }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
+};
+
+CrossClockIn();
