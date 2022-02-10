05ddcb0a1e0e1626f5a2afc1624cc4e128dc96a5
Enchi <enchi850404@gmail.com>
2022-02-10 09:54:27 +0800

    chore:tidy up script

--- Web_APItesting_dev.js
+++ Web_APItesting_dev.js
@@ -2,18 +2,10 @@ const newman = require('newman'); // require newman in your project
 const { lineNotify } = require("./api/lineNotify");
 // call newman.run to pass `options` object and wait for callback
 async function CrossClockIn() {
-    newman.run({
-        collection: require('./collection/HousekeeperToken.postman_collection.json'),
-        reporters: ['junit', 'cli'],
-        reporter: { junit: { export: "./newman" } }
-    }, function (err) {
-        if (err) { throw err; }
-        console.log('collection run complete!');
 
-    });
-
+    // 跨案場打卡API測試
     newman.run({
-        collection: require('./collection/CrossClockIn.postman_collection.json'),
+        collection: require('./collection/CrossClockIn_API_testing.postman_collection.json'),
         reporters: ['junit', 'cli'],
         reporter: { junit: { export: "./newman" } }
     }, function (err) {
@@ -21,6 +13,7 @@ async function CrossClockIn() {
         console.log('collection run complete!');
     });
 
+    // 區權會API測試 
     newman.run({
         collection: require('./collection/UnitOwnerAsssembly_API_testing.postman_collection.json'),
         environment: require('./environment/uoa-dev.postman_environment.json'),
@@ -30,6 +23,7 @@ async function CrossClockIn() {
         if (err) { throw err; }
         console.log('collection run complete!');
     });
+    
 };
 
 CrossClockIn();
