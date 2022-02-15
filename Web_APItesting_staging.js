d24c23368d46b6292556284cafdc1a3eb2e7510b
Enchi <enchi850404@gmail.com>
2022-02-15 17:35:55 +0800

    feat:Create Staging Enviorment and Modify APItest Script

---
+++ Web_APItesting_staging.js
@@ -0,0 +1,29 @@
+const newman = require('newman');
+const { lineNotify } = require("./api/lineNotify"); // LineNotify API
+
+async function Web_APItesting_staging() {
+
+    // 跨案場打卡API測試
+    newman.run({
+        collection: require('./collection/CrossClockIn_API_testing.postman_collection.json'),
+        environment: require('./environment/Staging.postman_environment.json'),
+        reporters: ['junit', 'cli'],
+        reporter: { junit: { export: "./newman" } }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
+
+    // 區權會API測試 
+    newman.run({
+        collection: require('./collection/UnitOwnerAsssembly_API_testing.postman_collection.json'),
+        environment: require('./environment/Staging.postman_environment.json'),
+        reporters: ['junit', 'cli'],
+        reporter: { junit: { export: "./newman" } }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
+};
+
+Web_APItesting_staging();
