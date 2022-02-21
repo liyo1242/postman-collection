fc968e94582576f795237fb6d93756b938be1f93
Enchi <enchi850404@gmail.com>
2022-02-21 18:23:46 +0800

    feat:建立prod環境

--- Web_APItesting_staging.js
+++ Web_APItesting_staging.js
@@ -4,20 +4,20 @@ const { lineNotify } = require("./api/lineNotify"); // LineNotify API
 async function Web_APItesting_staging() {
 
     // 跨案場打卡API測試
-    newman.run({
-        collection: require('./collection/CrossClockIn_API_testing.postman_collection.json'),
-        environment: require('./environment/Staging.postman_environment.json'),
-        reporters: ['junit', 'cli'],
-        reporter: { junit: { export: "./newman" } }
-    }, function (err) {
-        if (err) { throw err; }
-        console.log('collection run complete!');
-    });
+    // newman.run({
+    //     collection: require('./collection/CrossClockIn_API_testing.postman_collection.json'),
+    //     environment: require('./environment/Staging.postman_environment.json'),
+    //     reporters: ['junit', 'cli'],
+    //     reporter: { junit: { export: "./newman" } }
+    // }, function (err) {
+    //     if (err) { throw err; }
+    //     console.log('collection run complete!');
+    // });
 
     // 區權會API測試 
     newman.run({
-        collection: require('./collection/UnitOwnerAsssembly_API_testing.postman_collection.json'),
-        environment: require('./environment/Staging.postman_environment.json'),
+        collection: require('./collection/UnitOwnerAsssembly_Prod_API_testing.postman_collection.json'),
+        environment: require('./environment/Prod.postman_environment.json'),
         reporters: ['junit', 'cli'],
         reporter: { junit: { export: "./newman" } }
     }, function (err) {
