b9ba6da15a55f2bc8f0d3bc839f2e11b85088465
Enchi <enchi850404@gmail.com>
2022-02-22 15:47:58 +0800

    feat:設置staging,prod環境且可正確執行

---
+++ Web_APItesting_prod.js
@@ -0,0 +1,29 @@
+const newman = require('newman');
+const { lineNotify } = require("./api/lineNotify"); // LineNotify API
+
+async function Web_APItesting_prod() {
+
+    // 跨案場打卡API測試
+    newman.run({
+        collection: require('./collection/CrossClockIn_API_testing.postman_collection.json'),
+        environment: require('./environment/Prod.postman_environment.json'),
+        reporters: ['junit', 'cli'],
+        reporter: { junit: { export: "./newman" } }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
+
+    // 區權會API測試 
+    newman.run({
+        collection: require('./collection/UnitOwnerAsssembly_Prod_API_testing.postman_collection.json'),
+        environment: require('./environment/Prod.postman_environment.json'),
+        reporters: ['junit', 'cli'],
+        reporter: { junit: { export: "./newman" } }
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
+};
+
+Web_APItesting_prod();
