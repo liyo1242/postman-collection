b8e11eb7bf905fbec1482b21cf760ae1c12d8d22
enchilo <enchi850404@gmail.com>
2022-04-01 00:23:57 +0800

    feat:區權會編輯模式API測試撰寫 & 刪除棄用API

--- Web_APItesting_dev.js
+++ Web_APItesting_dev.js
@@ -3,16 +3,16 @@ const { lineNotify } = require("./api/lineNotify"); // LineNotify API
 
 async function Web_APItesting_dev() {
 
-    // 跨案場打卡_異常打卡API測試
-    newman.run({
-        collection: require('./collection/ManagerClock_API_testing.postman_collection'),
-        environment: require('./environment/Dev.postman_environment.json'),
-        reporters: ['junit', 'cli'],
-        reporter: { junit: { export: "./newman/ManagerClock.xml" } }
-    }, function (err) {
-        if (err) { throw err; }
-        console.log('collection run complete!');
-    });
+    // // 跨案場打卡_異常打卡API測試
+    // newman.run({
+    //     collection: require('./collection/ManagerClock_API_testing.postman_collection'),
+    //     environment: require('./environment/Dev.postman_environment.json'),
+    //     reporters: ['junit', 'cli'],
+    //     reporter: { junit: { export: "./newman/ManagerClock.xml" } }
+    // }, function (err) {
+    //     if (err) { throw err; }
+    //     console.log('collection run complete!');
+    // });
 
     // 區權會API測試 
     newman.run({
@@ -25,16 +25,16 @@ async function Web_APItesting_dev() {
         console.log('collection run complete!');
     });
 
-    // 實名制帳號綁定卡號API測試
-    newman.run({
-        collection: require('./collection/AuthorizationCarrier_API_testing.postman_collection.json'),
-        environment: require('./environment/Dev.postman_environment.json'),
-        reporters: ['junit', 'cli'],
-        reporter: { junit: { export: "./newman/AuthorizationCarrier.xml" } }
-    }, function (err) {
-        if (err) { throw err; }
-        console.log('collection run complete!');
-    });
+    // // 實名制帳號綁定卡號API測試
+    // newman.run({
+    //     collection: require('./collection/AuthorizationCarrier_API_testing.postman_collection.json'),
+    //     environment: require('./environment/Dev.postman_environment.json'),
+    //     reporters: ['junit', 'cli'],
+    //     reporter: { junit: { export: "./newman/AuthorizationCarrier.xml" } }
+    // }, function (err) {
+    //     if (err) { throw err; }
+    //     console.log('collection run complete!');
+    // });
 };
 
 Web_APItesting_dev();
